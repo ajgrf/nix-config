213d38dfe156be115bebf671d66dc22a1c305560
Alex Griffin <a@ajgrf.com>
2021-07-17 16:50:41 -0500

    Port to flake-utils-plus

--- flake.nix
+++ flake.nix
@@ -1,60 +1,43 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
   inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus";
   inputs.kmonad = {
     url = "github:kmonad/kmonad?dir=nix";
     flake = false;
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, kmonad }:
-    let
-      config = { allowUnfree = true; };
-      environments = flake-utils.lib.eachDefaultSystem (system: rec {
-        packages = import ./environments {
-          stable = import nixpkgs { inherit config system; };
-          unstable = import nixpkgs-unstable { inherit config system; };
-        };
-        legacyPackages = packages;
-        defaultPackage = packages.all-env;
-      });
+  outputs =
+    inputs@{ self, nixpkgs, nixpkgs-unstable, flake-utils-plus, kmonad }:
+    flake-utils-plus.lib.systemFlake {
+      inherit self inputs;
 
-      flakeSupport = ({ lib, pkgs, ... }: {
-        # Enable flake support.
-        nix.package = pkgs.nixFlakes;
-        nix.extraOptions = ''
-          experimental-features = nix-command flakes ca-references
-        '';
-        # Let 'nixos-version --json' know about the Git revision
-        # of this flake.
-        system.configurationRevision = lib.mkIf (self ? rev) self.rev;
-      });
+      channelsConfig.allowUnfree = true;
 
-      hosts = {
-        nixosConfigurations.iroh = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/iroh.nix
-            ./roles/desktop.nix
-            (import ./roles/kmonad kmonad)
-            flakeSupport
-          ];
+      channels.nixpkgs.input = nixpkgs;
+      channels.nixpkgs-unstable.input = nixpkgs-unstable;
+
+      packagesBuilder = { nixpkgs, nixpkgs-unstable, ... }:
+        import ./environments {
+          stable = nixpkgs;
+          unstable = nixpkgs-unstable;
         };
 
-        nixosConfigurations.poki = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [ ./hosts/poki.nix ./roles/desktop.nix flakeSupport ];
-        };
-
-        nixosConfigurations.tenzin = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [ ./hosts/tenzin.nix ./roles/desktop.nix flakeSupport ];
-        };
-
-        nixosConfigurations.petrus = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [ ./hosts/petrus.nix ./roles/nas flakeSupport ];
-        };
+      hostDefaults = {
+        system = "x86_64-linux";
+        modules = [ flake-utils-plus.nixosModules.saneFlakeDefaults ];
       };
-    in environments // hosts;
+
+      hosts.iroh.modules =
+        [ ./hosts/iroh.nix ./roles/desktop.nix (import ./roles/kmonad kmonad) ];
+
+      hosts.poki.modules = [ ./hosts/poki.nix ./roles/desktop.nix ];
+
+      hosts.tenzin.modules = [ ./hosts/tenzin.nix ./roles/desktop.nix ];
+
+      hosts.petrus = {
+        system = "aarch64-linux";
+        modules = [ ./hosts/petrus.nix ./roles/nas ];
+      };
+    };
 }
